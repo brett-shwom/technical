ee69518aecc1df2c038247e8a95acc5bbe74be5d
Brett Shwom <brett.shwom@nyu.edu>
2015-04-27 22:43:10 -0400

    settings.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports =  {
+    "consumerKey": "PUT_SOMETHING_IN_ME",
+    "consumerSecret": "PUT_SOMETHING_IN_ME",
+    "accessToken": "PUT_SOMETHING_IN_ME",
+    "accessTokenSecret": "PUT_SOMETHING_IN_ME"
+};
